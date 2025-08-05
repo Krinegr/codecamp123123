e9d7837dc7f4687287e7535ec3c9493c856aa6e3
Krinegr <zaikokrimil@gmail.com>
2025-08-05 11:24:41 +0500

    final

---
+++ hakatonbratan/Admin.py
@@ -0,0 +1,133 @@
+import asyncio
+import logging
+import os
+import re
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram import F
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters.command import Command
+import json
+router = Router()
+
+class Helper(StatesGroup):
+    Helper_input= State()
+    Delete_input=State()
+@router.message(Command("admin"))
+async def Is_user_admin(message: types.Message, state:FSMContext):
+    Hello_admin = "Здравствуйте администратор, чтобы войти в режим удаления напишите /deleter"
+    if message.from_user.id == 1427364974:
+        kb = [
+            [
+                types.KeyboardButton(text="Просмотреть запросы"),
+            ],
+        ]
+        keyboard = types.ReplyKeyboardMarkup(
+            keyboard=kb,
+            resize_keyboard=True,
+            input_field_placeholder="Поддержка компании ООО _Тмыв бабла_"
+        )
+        await message.answer(Hello_admin, reply_markup=keyboard)
+        return await state.set_state(Helper.Helper_input)
+
+    else:
+        await message.answer("Вы не администратор")
+        return None
+
+@router.message(Command("deleter"))
+async def IsUseradm(message: types.Message, state:FSMContext):
+    Hello_admin = "Вы вошли в режим удаления запросов, чтобы удалить информацию напишите: 'delete: ' + 'айди запроса', чтобы выйти из этого режима, напишите dedeleter"
+    if message.from_user.id == 1427364974:
+        await message.answer(Hello_admin)
+        return await state.set_state(Helper.Delete_input)
+@router.message(F.text.lower() == "просмотреть запросы")
+async def with_puree(message: types.Message):
+    if message.from_user.id == 1427364974:
+        with open("bdbd.json", 'r') as file:
+            data = json.load(file)
+
+        ids = [item["ID"] for item in data]
+        max_id = max(ids) if ids else 0
+
+        kb = [
+            [
+
+            ]
+        ]
+        for i in ids:
+            kb[0].append(types.KeyboardButton(text=str(i)))
+        keyboard = types.ReplyKeyboardMarkup(
+            keyboard=kb,
+            resize_keyboard=True,
+            input_field_placeholder="Поддержка компании ООО _Тмыв бабла_"
+        )
+        await message.answer("Все запросы в кнопках", reply_markup=keyboard)
+    else:
+        await message.answer("Вы не администратор")
+
+
+@router.message(F.text,Helper.Helper_input)
+async def handle_id_button(message: types.Message, state:FSMContext):
+        input_id = message.text
+        if input_id == "/deadmin":
+            await message.answer("Выход из режима администратора, чтобы вернуться, напишите /admin", reply_markup=types.ReplyKeyboardRemove())
+            await state.clear()
+            return
+        print(str(input_id))
+        input_id = int(input_id)
+        with open("bdbd.json", 'r') as file:
+            data = json.load(file)
+        item = next((item for item in data if item["ID"] == input_id), None)
+        if item:
+            response_text = item.get("text")
+            username = item.get("name")
+            await message.answer(f"Имя: {"@" + username}\nТекст: {response_text} , чтобы выйти из режима администратора напишите /deadmin")
+        else:
+            await message.answer("Запись не найдена.")
+
+
+@router.message(F.text,Helper.Delete_input)
+async def handle_id_button(message: types.Message, state:FSMContext):
+    text=message.text
+    if text == 'dedeleter':
+        await state.clear()
+        await message.answer('Вы вышли из режима удаления, чтобы вернуться, напишите /deleter')
+@router.message(Command("delete:"), Helper.Delete_input)
+async def delete_record(message: types.Message,state:FSMContext):
+    Id_To_Delete = int(message.text.split(": ")[1])
+    Id_To_Delete = Id_To_Delete - 1
+    with open("bdbd.json", "r") as file:
+        data = json.load(file)
+
+    data.pop(Id_To_Delete)
+    print(data)
+    with open('bdbd.json', 'w') as file:
+        json.dump(data, file, indent=4)
+        print("data:", data)
+    await message.answer(f"Запись с ID {Id_To_Delete + 1} успешно удалена. Чтобы вернуться в режим удаления напишите /deleter")
+    await state.clear()
+"""
+        with open("bdbd.json", 'r') as file:
+            bata = json.load(file)
+        result = '\n'.join([json.dumps(entry, indent=4) for entry in bata])
+        await message.reply(result)
+        print(result)
+"""
+
+"""
+    with open("bdbd.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    new_data = [item for item in data if item["ID"] != args]
+    new_data = []
+    for g in data:
+        if g in data:
+            data.remove(g)
+
+    if len(new_data) == len(data):
+        await message.answer(f"Запись с ID {args} не найдена.")
+        return
+
+    with open("bdbd.json", "w", encoding="utf-8") as file:
+        json.dump(new_data, file, indent=4, ensure_ascii=False)
+
+"""
---
+++ hakatonbratan/igramtestnew.py
@@ -0,0 +1,125 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram import F
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters.command import Command
+import json
+import Admin
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="Token here")
+dp = Dispatcher()
+router = Router
+
+async def main():
+    dp.include_routers(Admin.router)
+    await dp.start_polling(bot)
+
+class Message_to_group(StatesGroup):
+    help_input = State()
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message,state:FSMContext):
+    kb = [
+        [
+            types.KeyboardButton(text="FAQ"),
+            types.KeyboardButton(text="Помогите")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Поддержка компании ООО _Тмыв бабла_"
+    )
+    await message.answer("Вы написали в поддержку компании ООО _Тмыв бабла_, выберите нужную вам опцию", reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "faq")
+async def with_puree(message: types.Message):
+    await message.reply("Если сайт не работает - перезагрузите ваш компьютер. Если FAQ оказалась недостаточной, просто напишите Помогите", reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message(F.text.lower() == "помогите")
+async def without_puree(message: types.Message,state:FSMContext):
+    await message.reply("Подробно опишите вашу проблему, укажите контакты, по которым можно с вами связаться и в ближайшее время с вами свяжется наш администратор", reply_markup=types.ReplyKeyboardRemove())
+    global schet
+    schet += 1
+    return await state.set_state(Message_to_group.help_input)
+schet = 0
+@dp.message(Message_to_group.help_input)
+async def obrabotka_zaprosa(message: types.Message, state: FSMContext ):
+    text = message.text
+    data = await state.get_data()
+    if text != "Стоп":
+        await message.reply(message.text)
+        mass = data.get("message_saved", [])
+        mass.append(text)
+        await state.update_data(message_saved = mass)
+    else:
+        await message.reply("Отправлено")
+        mass = data.get("message_saved")
+        await message.reply("\n".join(mass))
+        textfromuser = "\n".join(mass)
+        username = message.from_user.username
+        spisok = {
+            "ID": schet,
+            "text": textfromuser,
+            "name": username
+        }
+        if os.path.exists("bdbd.json") and os.path.getsize("bdbd.json") > 0:
+            with open("bdbd.json", 'r') as file:
+                bata = json.load(file)
+        else:
+            bata = []
+        bata.append(spisok)
+        with open('bdbd.json', 'w') as file:
+            json.dump(bata, file, indent=4)
+        print(bata)
+        print(schet)
+        await state.clear()
+        #messageforgroup = "\n".join(mass)
+        await bot.send_message(-4681080381, "Отправили новый запрос о помощи, чтобы просмотреть его, откройте бота и напишите /admin    " + "\nИмя пользователя: @" + message.from_user.username)
+        #await bot.send_message(-4681080381, messageforgroup + "\nИмя пользователя: @" + message.from_user.username)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    """
+            global schet
+        schet += 1
+        await message.reply("Отправлено")
+        mass = data.get("message_saved")
+        await message.reply("\n".join(mass))
+        textfromuser = "".join(mass)
+        username = message.from_user.username
+        spisok = {"ID": schet,
+                  "text": textfromuser,
+                  "name": username}
+        with open("bdbd.json", 'r') as file:
+            bata = json.load(file)
+        bata.update(spisok)
+        print(bata)
+        with open('bdbd.json', 'w') as file:
+            json.dump(bata, file, indent=4)
+    """
+    """
+    global schet
+        schet += 1
+        await message.reply("Отправлено")
+        mass = data.get("message_saved")
+        await message.reply("\n".join(mass))
+        textfromuser = "".join(mass)
+        username = message.from_user.username
+        spisok = {"ID": schet,
+                  "text": textfromuser,
+                  "name": username}
+        with open('bdbd.json', 'r') as file:
+            if schet == 1:
+                What_In_File = spisok
+            else:
+                What_In_File = (json.load(file))
+        print(What_In_File)
+        print(spisok)
+        with open('bdbd.json', 'w') as file:
+                json.dump(What_In_File, file, indent=4)
+    """
